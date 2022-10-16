05240ad5e60e4c56ba0ee8dfe3c67e634eab892b
Mwanziu <stephaniemwanziu1@gmail.com>
2022-10-16 10:17:56 -0700

    Ammended formatting to fit betty style

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the entire alphabet in lowercase,
+ *main - prints the entire alphabet in lowercase,
  *except q and e
  *followed by a new line,
  * Return: Aways 0 (Sucess)
@@ -10,6 +10,7 @@
 int main(void)
 {
 	char ch = 'a';
+
 	while (ch <= 'z')
 	{
 		if (ch != 'e' && ch != 'q')
